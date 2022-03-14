4248e2aafa989b72e987a314c9216a1a9254f03c
Yuliya Ivanova <55700075+ivanova1503@users.noreply.github.com>
2022-03-14 08:15:59 +0200

    Added first 3 tests (#1)
    * added SimpleTest
    
    * SimpleTest2.java
    
    * SimpleTest3.java

---
+++ src/test/java/lesson11_git/SimpleTest.java
@@ -0,0 +1,11 @@
+package lesson11_git;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void assertTrueTest() {
+        Assertions.assertTrue(3>2);
+    }
+}
---
+++ src/test/java/lesson11_git/SimpleTest2.java
@@ -0,0 +1,11 @@
+package lesson11_git;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest2 {
+    @Test
+    void assertTrueTest() {
+        Assertions.assertTrue(3>2);
+    }
+}
