42a368dfc0477d5c63e12e638e7d67eda825db2f
ivanova1503 <ivanova1503@gmail.com>
2022-03-14 08:44:07 +0200

    change test

--- src/test/java/lesson11_git/SimpleTest.java
+++ src/test/java/lesson11_git/SimpleTest.java
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Test;
 public class SimpleTest {
     @Test
     void assertTrueTest() {
-        Assertions.assertTrue(3 > 1);
+        Assertions.assertFalse(3 > 1);
     }
 }
